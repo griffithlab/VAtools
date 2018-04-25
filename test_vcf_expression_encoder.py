f9e8e5572624518a142d63742eb01249352a4617
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-04-25 11:09:43 -0500

    Add tests for command line parser rules

--- test_vcf_expression_encoder.py
+++ test_vcf_expression_encoder.py
@@ -11,3 +11,27 @@ class VcfExpressionEncoderTests(unittest.TestCase):
 
     def test_source_compiles(self):
         self.assertTrue(py_compile.compile(self.executable))
+
+    def test_error_custom_format_id_column_not_set(self):
+        with self.assertRaises(Exception) as context:
+            command = [
+                os.path.join(self.test_data_dir, 'input.vcf'),
+                os.path.join(self.test_data_dir, 'genes.fpkm_tracking'),
+                'custom',
+                'gene',
+                '-e', 'FPKM',
+            ]
+            vcf_expression_encoder.main(command)
+        self.assertTrue('--id-column is not set. This is required when using the `custom` format.' == str(context.exception))
+
+    def test_error_custom_format_expression_column_not_set(self):
+        with self.assertRaises(Exception) as context:
+            command = [
+                os.path.join(self.test_data_dir, 'input.vcf'),
+                os.path.join(self.test_data_dir, 'genes.fpkm_tracking'),
+                'custom',
+                'gene',
+                '-i', 'tracking_id',
+            ]
+            vcf_expression_encoder.main(command)
+        self.assertTrue('--expression-column is not set. This is required when using the `custom` format.' == str(context.exception))
