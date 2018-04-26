8f81fb995f5e9fd4e936c2d2045cb693dbce0dea
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-04-26 09:26:58 -0500

    Add classifiers and copyright information to setup.py

--- setup.py
+++ setup.py
@@ -14,4 +14,22 @@ setup(
         'pandas',
         'gtfparse',
     ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+
+        "Programming Language :: Python :: 3.5"
+
+        'License :: OSI Approved :: MIT License',
+    ],
+
+    author = "Susanna Kiwala",
+    author_email = "ssiebert@wustl.edu",
+    description = "A tool for annotating VCF files with expression data",
+    license = "MIT License",
+    keywords = "cancer sequencing variant variants gene expression transcript VCF",
+    #This needs to be the url where the code is being hosted
+    url = "https://github.com/griffithlab/vcf-expression-encoder",
 )
