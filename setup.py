e02720f15b55fc8023d867ed3d679dfc66105549
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-08-10 10:14:45 -0500

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="vcf-annotation-tools",
-    version="1.0.0",
+    version="1.1.0",
     packages=["vcf_annotation_tools"],
     entry_points={
         "console_scripts":[
